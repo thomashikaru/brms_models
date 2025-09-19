5ab0fa805861ed75b60eedcc887b5712a448b03c
anonymous <thclark@mit.edu>
2025-09-19 10:25:28 -0400

    reg in

---
+++ scripts/ncgp_RegInExcl_haserror_20250919.R
@@ -0,0 +1,31 @@
+library(brms)
+
+rstan::rstan_options(auto_write = TRUE)
+
+dat <- readRDS("/om2/user/thclark/brms_models/data/ncgp_data_20250917.rds")
+
+# Define priors for Bayesian models
+priors <- c(
+    prior(normal(0, 0.1), class = "b"),
+    prior(normal(0, 1), class = "Intercept")
+)
+
+if (file.exists("/om2/user/thclark/brms_models/outputs/fitted_models/ncgp_RegInExcl_haserror_20250919.rds")) {
+    cat("Model already exists.")
+} else {
+    m <- brm(
+        RegIn_excl ~ log_freq + word_nchar + surprisal + word_num_in_sent + has_error_posterior + pos_tag + Region * plausibility +
+            (log_freq + word_nchar + surprisal + word_num_in_sent + has_error_posterior || submission_id) +
+            (1 || Item),
+        data = dat,
+        family = bernoulli(),
+        backend = "rstan",
+        prior = priors,
+        chains = 4,
+        cores = 4,
+        seed = 123,
+        refresh = 1
+    )
+
+    saveRDS(m, file = "/om2/user/thclark/brms_models/outputs/fitted_models/ncgp_RegInExcl_haserror_20250919.rds")
+}
