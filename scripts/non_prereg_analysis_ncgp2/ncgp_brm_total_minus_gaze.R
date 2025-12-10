7a075fb6b0692c7d7e7cce41a48d2e3a4a42721c
anonymous <thclark@mit.edu>
2025-12-10 13:45:11 -0500

    total minus gaze

--- scripts/non_prereg_analysis_ncgp2/ncgp_brm_total_minus_gaze.R
+++ scripts/non_prereg_analysis_ncgp2/ncgp_brm_total_minus_gaze.R
@@ -2,7 +2,7 @@ library(brms)
 
 rstan::rstan_options(auto_write = TRUE)
 
-dat <- readRDS("/om2/user/thclark/brms_models/data/ncgp2_data_20251110.rds")
+dat <- readRDS("/orcd/data/rplevy/001/om2/thclark/brms_models/data/ncgp2_data_20251110.rds")
 
 # Define priors for Bayesian models
 priors <- c(
@@ -11,9 +11,9 @@ priors <- c(
     prior(normal(0, 1), class = "Intercept")
 )
 
-model_name <- "ncgp2_prereg_total_minus_gaze"
+model_name <- "ncgp_total_minus_gaze"
 
-if (file.exists(paste0("/om2/user/thclark/brms_models/outputs/fitted_models/", model_name, ".rds"))) {
+if (file.exists(paste0("/orcd/data/rplevy/001/om2/thclark/brms_models/outputs/fitted_models/", model_name, ".rds"))) {
     cat("Model already exists.")
 } else {
     m <- brm(
@@ -33,5 +33,5 @@ if (file.exists(paste0("/om2/user/thclark/brms_models/outputs/fitted_models/", m
         refresh = 1
     )
 
-    saveRDS(m, file = paste0("/om2/user/thclark/brms_models/outputs/fitted_models/", model_name, ".rds"))
+    saveRDS(m, file = paste0("/orcd/data/rplevy/001/om2/thclark/brms_models/outputs/fitted_models/", model_name, ".rds"))
 }
