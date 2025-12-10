938edeaec3a9f2b1545890f335a6eaf728c2cb0e
anonymous <thclark@mit.edu>
2025-12-10 15:03:26 -0500

    maximal brms model

---
+++ scripts/non_prereg_analysis_ncgp2/ncgp_brm_total_minus_gaze_maximal.R
@@ -0,0 +1,37 @@
+library(brms)
+
+rstan::rstan_options(auto_write = TRUE)
+
+dat <- readRDS("/orcd/data/rplevy/001/om2/thclark/brms_models/data/ncgp2_data_20251110.rds")
+
+# Define priors for Bayesian models
+priors <- c(
+    prior(normal(0, 0.1), class = "b"),
+    prior(normal(0, 0.1), class = "b", dpar = "hu"),
+    prior(normal(0, 1), class = "Intercept")
+)
+
+model_name <- "ncgp_total_minus_gaze_maximal"
+
+if (file.exists(paste0("/orcd/data/rplevy/001/om2/thclark/brms_models/outputs/fitted_models/", model_name, ".rds"))) {
+    cat("Model already exists.")
+} else {
+    m <- brm(
+        bf(
+            rt_total_minus_gaze ~ log_freq + word_nchar + surprisal_nc + word_num_in_sent + pos_tag + Region * cond_id +
+                (log_freq + word_nchar + surprisal_nc + word_num_in_sent + pos_tag + Region * cond_id || submission_id) +
+                (log_freq + word_nchar + surprisal_nc + word_num_in_sent + pos_tag + Region * cond_id || Item),
+            hu ~ log_freq + word_nchar + surprisal_nc + word_num_in_sent + pos_tag + Region * cond_id
+        ),
+        data = dat,
+        family = hurdle_lognormal(),
+        backend = "rstan",
+        prior = priors,
+        chains = 4,
+        cores = 4,
+        seed = 123,
+        refresh = 1
+    )
+
+    saveRDS(m, file = paste0("/orcd/data/rplevy/001/om2/thclark/brms_models/outputs/fitted_models/", model_name, ".rds"))
+}
